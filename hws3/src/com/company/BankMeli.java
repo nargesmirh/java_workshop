c3850d47672152db4cfd0be9bf1655a10a25599e
Your Name <you@example.com>
2020-10-09 22:07:12 +0330

    project3

---
+++ hws3/src/com/company/BankMeli.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class BankMeli extends BankMarkazi{
+    public void Profit(){
+        System.out.println("**profit's melibank and ayande is 18% in this year**");
+
+    }
+
+    @Override
+    public void Harvest() {
+        super.Harvest();
+    }
+
+    @Override
+    public void Deposit() {
+        super.Deposit();
+    }
+
+    @Override
+    public void Cashoperations() {
+        super.Cashoperations();
+    }
+    @Override
+    public void Transitionoperations() {
+        super.Transitionoperations();
+    }
+
+    @Override
+    public void Cardtocardoperation() {
+        super.Cardtocardoperation();
+    }
+
+    @Override
+    public void Cheque() {
+        super.Cheque();
+    }
+}
