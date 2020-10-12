fa8111aa30007eaf7980cb512e2e547e43d7c277
nargesmirh <71467063+nargesmirh@users.noreply.github.com>
2020-10-12 23:20:04 +0330

    hw3

---
+++ untitled4/src/com/company/BankAyande.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class BankAyande extends BankMarkazi{
+    public void Bankstock(){
+        System.out.println("**Ayandeh Bank, in order to strengthen the relationship between the money and capital markets and provide better services to customers who are active in the capital market or intend to buy and sell stocks and securities on the stock exchange; Has launched two new products, \"Connected Account\" and \"Pickup System\", which are briefly explained in this regard:\n" +
+                "A connected bank account is an account from which the power of attorney has been issued to the brokerage of Ayandeh Bank or other brokerages that are parties to the bank's contract. This bank account is connected to the online account of the brokerage clients. Thus, at the time of ordering the purchase of shares or securities by the customer in the online trading system of securities, the amount of the order (without limitation) is deducted directly from the connected bank account and the purchase is made. In this way, there is no need to increase the credit of the account in the brokerage and the withdrawal of money to send the purchase order is done automatically from the connected bank account.**");
+
+    }
+
+    @Override
+    public void Harvest() {
+        super.Harvest();
+    }
+
+    @Override
+    public void Deposit() {
+        super.Deposit();
+    }
+
+    @Override
+    public void Cashoperations() {
+        super.Cashoperations();
+    }
+    @Override
+    public void Transitionoperations() {
+        super.Transitionoperations();
+    }
+
+    @Override
+    public void Cardtocardoperation() {
+        super.Cardtocardoperation();
+    }
+
+    @Override
+    public void Cheque() {
+        super.Cheque();
+    }
+}
---
+++ untitled4/src/com/company/BankSepah.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class BankSepah extends BankMarkazi{
+
+    public void Loan(){
+        System.out.println("**this bank has a very good loan to customer**");
+        if(holding>10000){
+            System.out.println("loan belong to customer");
+        }
+        else{
+            System.out.println("increas your holding:)");
+        }
+
+    }
+
+    @Override
+    public void Harvest() {
+        super.Harvest();
+    }
+
+    @Override
+    public void Deposit() {
+        super.Deposit();
+    }
+
+    @Override
+    public void Cashoperations() {
+        super.Cashoperations();
+    }
+
+    @Override
+    public void Transitionoperations() {
+        super.Transitionoperations();
+    }
+
+    @Override
+    public void Cardtocardoperation() {
+        super.Cardtocardoperation();
+    }
+
+    @Override
+    public void Cheque() {
+        super.Cheque();
+    }
+
+
+}
